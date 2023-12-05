d5561187527a0af7dd9ff44ed4a4f6a1900784ca
Shakana <shakana.jamal.moalin@avanade.com>
2023-12-05 19:19:36 +0100

    reuploding files

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php
+// FILEPATH: /c:/xampp/htdocs/wordpress/wp-content/themes/avatsweden/404.php
+
+// Set the HTTP response code to 404
+http_response_code(404);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>404 Not Found</title>
+</head>
+<body>
+    <h1>404 Not Found</h1>
+    <p>The page you are looking for could not be found.</p>
+</body>
+</html>
