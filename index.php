0a20222654dfd1e1aed19e0f172661b2bcaa06e2
Shakana <shakana.jamal.moalin@avanade.com>
2023-12-05 19:07:02 +0100

    committing all files

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+<main>
+    <section class="input-section">
+        <form action="" method="post">
+            <input type="text" name="url" id="url" placeholder="Insert URL* We may contact" class="input">
+            <input type="submit" value="Start Test" class="submit-btn">
+        </form>
+        <div class="divider"></div>
+    </section>
+    <section class="text-slide">
+        <h3>“Green, fast & energy-efficient websites”</h3>
+    </section>
+</main>
+<?php get_footer(); ?>
